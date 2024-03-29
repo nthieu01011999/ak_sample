42c4f5f5bdfd81726139a48d53d64755fb89c9b1
nthieu01011999@gmail.com <nthieu01011999@gmail.com>
2024-03-29 12:01:40 +0700

    Initial commit

---
+++ sourcebase/cross-compile/rts39xx_sdk_v5.0.1/toolchain/asdk-10.3.1-a7-EL-5.4-u1.0-a32nh-220218/lib/task_webrtc.cpp
@@ -0,0 +1,59 @@
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <vector>
+
+#include <algorithm>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <random>
+#include <stdexcept>
+#include <thread>
+#include <unordered_map>
+#include <chrono>
+#include <pthread.h>
+
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <atomic>
+
+#include "app.h"
+#include "app_dbg.h"
+#include "task_list.h"
+
+using namespace std;
+
+q_msg_t gw_task_webrtc_mailbox;
+
+void *gw_task_webrtc_entry(void *) {
+	ak_msg_t *msg = AK_MSG_NULL;
+
+	wait_all_tasks_started();
+
+
+	APP_DBG("[STARTED] gw_task_webrtc_entry\n");
+
+	while (1) {
+		/* get messge */
+		msg = ak_msg_rev(GW_TASK_WEBRTC_ID);
+
+		switch (msg->header->sig) {
+			
+		default:
+		break;
+		}
+
+		/* free message */
+		ak_msg_free(msg);
+	}
+
+	return (void *)0;
+}
